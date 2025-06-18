555ebee4e85f597be7d07ea48dd4f90b3231b03d
shruti <shrutisinghparihar1102@gmail.com>
2025-06-18 06:32:50 +0530

    concepts of numpy

---
+++ numpy-tut.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+myarr = np.array([3, 4, 5])                                #numpy array
+print(myarr)
+
+listArr = np.array([[1,2,3], [4,2,6], [8, 3, 5]])          #numpy array from list
+print(listArr)
+print(listArr.shape)
+print(listArr.size)
+
+rng = np.arange(10)                                        #a range: creates numpy array from 0 to n-1
+print(rng)
