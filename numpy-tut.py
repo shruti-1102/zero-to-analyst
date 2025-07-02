6980376b536f52f8d04ba16caad18207b7cb413e
shruti <shrutisinghparihar1102@gmail.com>
2025-07-02 23:45:09 +0530

    updated file

--- numpy-tut.py
+++ numpy-tut.py
@@ -3,7 +3,7 @@ import numpy as np
 myarr = np.array([3, 4, 5])                                #numpy array
 print(myarr)
 
-listArr = np.array([[1,2,3], [4,2,6], [8, 3, 5]])          #numpy array from list
+listArr = np.array([[1,2,3], [4,2,6], [8, 3, 5]])          #numpy array from lists
 print(listArr)
 print(listArr.shape)
 print(listArr.size)
